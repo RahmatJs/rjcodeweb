5c80cb8149ac103acc48602d06a0c57c9ddf8496
Rahmat Juliansah <ahmatjuliansah@gmail.com>
2026-02-08 13:11:42 +0700

    update fitur

--- script/script.js
+++ script/script.js
@@ -182,6 +182,21 @@ btnCloseCategory.addEventListener("click", () => {
       }
   });
 
+  // PREV NEXT SIDEBAR POPULAR
+  const rightPopular = document.getElementById("right-popular");
+  const leftPopular = document.getElementById("left-popular");
+  const popularBox1 = document.querySelector(".popular-box1");
+  const popularBox2 = document.querySelector(".popular-box2");
+
+  rightPopular.addEventListener("click", () => {
+    popularBox1.classList.add("popOpac1");
+    popularBox2.classList.add("popOpac2");
+  });
+  leftPopular.addEventListener("click", () => {
+    popularBox1.classList.remove("popOpac1");
+    popularBox2.classList.remove("popOpac2");
+  });
+
     // copy url otomatis
   const copyBtn = document.getElementById("copy-url");
   const originalContent = copyBtn.innerHTML;
